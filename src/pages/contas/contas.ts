e641d28b3bbb9d66ae381ce7658ab1c1915d7d04
Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>
2018-06-28 10:00:18 -0400

    [ADJUST] alert for the delete and toast add
    Signed-off-by: Walter Nascimento Barroso <walter.nascimento.barroso@gmail.com>

--- src/pages/contas/contas.ts
+++ src/pages/contas/contas.ts
@@ -1,5 +1,11 @@
 import { Component } from "@angular/core";
-import { ModalController, NavController, NavParams } from "ionic-angular";
+import {
+    AlertController,
+    ModalController,
+    NavController,
+    NavParams,
+    ToastController
+} from "ionic-angular";
 import { ModalContasPage } from "../modal-contas/modal-contas";
 import { DAOContas } from "../../dao/dao-contas";
 
@@ -12,17 +18,30 @@ export class ContasPage {
     listContas;
 
     constructor(
+        public alertCtrl: AlertController,
         public modalCtrl: ModalController,
         public navCtrl: NavController,
-        public navParams: NavParams
+        public navParams: NavParams,
+        public toastCtrl: ToastController
     ) {
         this.listContas = this.dao.getList();
+    }
+
+    toast(msg) {
+        const toast = this.toastCtrl.create({
+            message: msg,
+            duration: 3000,
+            showCloseButton: true,
+            closeButtonText: "Ok"
+        });
+        toast.present();
     }
 
     insert() {
         const modal = this.modalCtrl.create(ModalContasPage);
         modal.onDidDismiss(data => {
             this.dao.insert(data);
+            this.toast("Conta Criada");
         });
         modal.present();
     }
@@ -33,11 +52,26 @@ export class ContasPage {
         });
         modal.onDidDismiss(data => {
             this.dao.edit(data);
+            this.toast("Conta Editada");
         });
         modal.present();
     }
 
     delete(conta) {
-        this.dao.delete(conta);
+        const prompt = this.alertCtrl.create({
+            title: "Excluir Conta",
+            message: "Tem certeza que deseja excluir a conta?",
+            buttons: [
+                { text: "Cancelar" },
+                {
+                    text: "Excluir",
+                    handler: data => {
+                        this.dao.delete(conta);
+                        this.toast("Conta Excluida");
+                    }
+                }
+            ]
+        });
+        prompt.present();
     }
 }
